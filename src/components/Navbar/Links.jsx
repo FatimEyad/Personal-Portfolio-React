91e764e8b98a31ed3facc99174124cb0fcbb2675
FatimEyad <fatimaalhamed626@gmail.com>
2023-05-31 22:52:21 +0300

    .

--- src/components/Navbar/Links.jsx
+++ src/components/Navbar/Links.jsx
@@ -11,7 +11,7 @@ const Links = ({trigger}) => {
             <li><a href="/" style={{ color: darkMode ? "white" : "" }}>Home</a></li>
             <li><a href="#About" style={{ color: darkMode ? "white" : "" }}>About</a></li>
             <li><a href="#Portfolio" style={{ color: darkMode ? "white" : "" }}>Portfolio</a></li>
-            <li><a href="#Projects" style={{ color: darkMode ? "white" : "" }}>Projects</a></li>
+            {/* <li><a href="#Projects" style={{ color: darkMode ? "white" : "" }}>Projects</a></li> */}
             <li><a href="#services" style={{ color: darkMode ? "white" : "" }}>Services</a></li>
             <li><a href="#testimonial" style={{ color: darkMode ? "white" : "" }}>Testimonial</a></li>
             <li><a href="#contact" style={{ color: darkMode ? "white" : "" }}>Contact</a></li>
